db84edb23f6825d52582272f8a623b25a77702a0
RafaelMurrieta <r7121005846@gmail.com>
2024-05-10 10:23:37 -0600

    arreglo de diseño y evento de busqueda

--- pruebas/01-reading-list/Rafael Murrieta/src/search.tsx
+++ pruebas/01-reading-list/Rafael Murrieta/src/search.tsx
@@ -1,12 +1,30 @@
-function search(){
-    return(
-        <>
-        <div className="container-search w-auto flex flex-wrap gap-10">
-            <input type="text" placeholder="Buscar" className="input-search rounded-md p-4"  id="search-book"/>
-            <button className="btn-search font-white">Buscar</button>
-        </div>
-        </>
-    )
+import React, { useState, ChangeEvent } from 'react';
+
+function Search() {
+  const [searchValue, setSearchValue] = useState('');
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+
+  const handleSearch = () => {
+    console.log("Valor de búsqueda:", searchValue);
+  };
+
+  return (
+    <div>
+      <div className="input-search flex gap-1">
+        <input
+          type="text"
+          placeholder="Buscar"
+          className="input-search rounded-md w-2/3 p-4"
+          id="search-book"
+          value={searchValue}
+          onChange={handleInputChange}
+        />
+        <button className="btn-search font-white w-1/4" onClick={handleSearch}>Buscar</button>
+      </div>
+    </div>
+  );
 }
 
-export default search;
+export default Search;
